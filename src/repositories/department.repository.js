95013ed9e20047fc1463fbf8a62d5cedfdf8a718
JDHCamacho12 <jdhcamachobusiness@gmail.com>
2026-03-17 21:38:34 -0500

    Fix SQL query to properly escape 'admin_gen'

--- src/repositories/department.repository.js
+++ src/repositories/department.repository.js
@@ -1,7 +1,7 @@
 const pool = require("../db/sql");
 
 async function find() {
-    const response = await pool.query("SELECT id, name FROM department where name != admin_gen ORDER BY name");
+    const response = await pool.query("SELECT id, name FROM department where name != 'admin_gen' ORDER BY name");
     return response.rows;
 }
 
